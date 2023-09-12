59e93149a05826c40919a45e46ad0be1095ec202
tyler davis <tyler.davis432@gmail.com>
2023-09-12 19:47:37 -0400

    syntax done

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int answer = 4;
+            string response;
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + " is greater than or equal to nine";
+            }
+
+
+            //inferred typing 
+            // string interpolation 
+            // ternary operator
+
+            var reponse = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater that or equal to nine";
+            Console.WriteLine(response);
+            
         }
     }
 }
